880dd68ff87277b4dc5208f03c5d6d48f43f5b87
zoso9999 <fil.iacobelli@stud.uniroma3.it>
2023-05-11 00:11:22 +0200

    zanzare si muovono

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 var svg = d3.select("body").append("svg")
     .attr("viewBox", "0 0 150 70")
-    .style("border", "2px solid black");   
+    .style("border", "2px solid black")
+    .attr("time", "1")
+    .on("click", moveMosquito);
 
-
-function drawMosquito(data) {
+function drawMosquito() {
     svg.selectAll("image")
-    .data(data)
+    .data(window.data)
     .enter()
     .append("svg:image")
     .attr("xlink:href", "zanzara.png")
-    .attr("x", function(d) { return d.x1; })
-    .attr("y", function(d) { return d.y1; })
+    .attr("x", function(d) { return d.x0; })
+    .attr("y", function(d) { return d.y0; })
     .attr("width", "5")
     .attr("height", "5")
-    .attr("id", function(d) { return d.id; })
-    .attr("onclick", "move()")
+    .attr("id", function(d) { return d.id; });
+    
+}
+
+function moveMosquito() {
+    var mosquitos = svg.selectAll("image")
+                    .data(window.data);
+    var time = svg.attr("time");
+    if (time%3 == 1) {
+        mosquitos.attr("x", function(d) { return d.x1; })
+                .attr("y", function(d) { return d.y1; })
+    }
+    else if (time%3 == 2) {
+        mosquitos.attr("x", function(d) { return d.x2; })
+                .attr("y", function(d) { return d.y2; })
+    }
+    else {
+        mosquitos.attr("x", function(d) { return d.x0; })
+                .attr("y", function(d) { return d.y0; })
+    }
+    svg.attr("time",parseInt(time)+1)
 }
 
 d3.json("data.json")
     .then(function(data) {
-    drawMosquito(data)
+    window.data = data;
+    drawMosquito();
 
 })
 .catch(function(error) {
