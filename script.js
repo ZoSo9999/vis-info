e56f7393412afe4dc932eeb016e18e62893398d8
zoso9999 <fil.iacobelli@stud.uniroma3.it>
2023-05-11 23:19:16 +0200

    finito

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
+var updateTime = 1000;
+var domain = [0,1000];
+
+var scaleX = d3.scaleLinear();
+scaleX.domain(domain);
+scaleX.range([10,140]);
+
+var scaleY = d3.scaleLinear();
+scaleY.domain(domain);
+scaleY.range([10,60]);
+
 var svg = d3.select("body").append("svg")
+    .attr("id","my-svg")
     .attr("viewBox", "0 0 150 70")
     .style("border", "2px solid black")
-    .attr("time", "1")
-    .on("click", moveMosquito);
+    .attr("time", "1");
 
-function drawMosquito() {
+
+
+
+
+function addEvent() {
+    const svgElement = document.getElementById('my-svg');
+    svgElement.addEventListener('click', (event) => {
+        if (event.target === svgElement) {
+            moveMosquito();
+        }
+    });
+}
+
+function drawMosquito(data) {
     svg.selectAll("image")
-    .data(window.data)
+    .data(data)
     .enter()
     .append("svg:image")
     .attr("xlink:href", "zanzara.png")
-    .attr("x", function(d) { return d.x0; })
-    .attr("y", function(d) { return d.y0; })
-    .attr("width", "5")
-    .attr("height", "5")
-    .attr("id", function(d) { return d.id; });
-    
+    .attr("x", function(d) { return scaleX(d.x0); })
+    .attr("y", function(d) { return scaleY(d.y0); })
+    .attr("width", "4")
+    .attr("height", "4")
+    .attr("dead", "false")
+}
+
+function killMosquito(){
+    var images = document.querySelectorAll("image");
+    for (let i=0; i<images.length; i++) {
+        images[i].addEventListener("click", function(e){
+            e.target.setAttribute("dead","true");
+        });
+    }
 }
 
 function moveMosquito() {
-    var mosquitos = svg.selectAll("image")
-                    .data(window.data);
+    var mosquitos = d3.selectAll("image[dead='false']")
+                    .transition()
+                    .duration(updateTime);
     var time = svg.attr("time");
     if (time%3 == 1) {
-        mosquitos.attr("x", function(d) { return d.x1; })
-                .attr("y", function(d) { return d.y1; })
+        mosquitos.attr("x", function(d) { return scaleX(d.x1); })
+                .attr("y", function(d) { return scaleY(d.y1); })
     }
     else if (time%3 == 2) {
-        mosquitos.attr("x", function(d) { return d.x2; })
-                .attr("y", function(d) { return d.y2; })
+        mosquitos.attr("x", function(d) { return scaleX(d.x2); })
+                .attr("y", function(d) { return scaleY(d.y2); })
     }
     else {
-        mosquitos.attr("x", function(d) { return d.x0; })
-                .attr("y", function(d) { return d.y0; })
+        mosquitos.attr("x", function(d) { return scaleX(d.x0); })
+                .attr("y", function(d) { return scaleY(d.y0); })
     }
     svg.attr("time",parseInt(time)+1)
 }
 
 d3.json("data.json")
     .then(function(data) {
-    window.data = data;
-    drawMosquito();
+        drawMosquito(data);
+        addEvent();
+        killMosquito();
 
 })
 .catch(function(error) {
